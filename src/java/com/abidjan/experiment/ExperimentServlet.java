fa4e179003b06dd567c131ad783a8be14d63ae1c
Brice Gnanago <gnanagobrice@gmail.com>
2021-02-02 12:13:29 +0000

    Manipuler une servlet - Information l'adresse IP et le Navigateur utilisé par le client

--- src/java/com/abidjan/experiment/ExperimentServlet.java
+++ src/java/com/abidjan/experiment/ExperimentServlet.java
@@ -12,6 +12,15 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.net.InetAddress;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.inject.Inject;
+import javax.inject.Provider;
+import javax.servlet.http.HttpSession;
+
+
 
 /**
  *
@@ -34,13 +43,76 @@ public class ExperimentServlet extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
+             String  browserDetails  =   request.getHeader("User-Agent");
+        String  userAgent       =   browserDetails;
+        String  user            =   userAgent.toLowerCase();
+
+        String os = "";
+        String browser = "";
+
+       
+        //=================OS=======================
+         if (userAgent.toLowerCase().indexOf("windows") >= 0 )
+         {
+             os = "Windows";
+         } else if(userAgent.toLowerCase().indexOf("mac") >= 0)
+         {
+             os = "Mac";
+         } else if(userAgent.toLowerCase().indexOf("x11") >= 0)
+         {
+             os = "Unix";
+         } else if(userAgent.toLowerCase().indexOf("android") >= 0)
+         {
+             os = "Android";
+         } else if(userAgent.toLowerCase().indexOf("iphone") >= 0)
+         {
+             os = "IPhone";
+         }else{
+             os = "UnKnown, More-Info: "+userAgent;
+         }
+         //===============Browser===========================
+        if (user.contains("msie"))
+        {
+            String substring=userAgent.substring(userAgent.indexOf("MSIE")).split(";")[0];
+            browser=substring.split(" ")[0].replace("MSIE", "IE")+"-"+substring.split(" ")[1];
+        } else if (user.contains("safari") && user.contains("version"))
+        {
+            browser=(userAgent.substring(userAgent.indexOf("Safari")).split(" ")[0]).split("/")[0]+"-"+(userAgent.substring(userAgent.indexOf("Version")).split(" ")[0]).split("/")[1];
+        } else if ( user.contains("opr") || user.contains("opera"))
+        {
+            if(user.contains("opera"))
+                browser=(userAgent.substring(userAgent.indexOf("Opera")).split(" ")[0]).split("/")[0]+"-"+(userAgent.substring(userAgent.indexOf("Version")).split(" ")[0]).split("/")[1];
+            else if(user.contains("opr"))
+                browser=((userAgent.substring(userAgent.indexOf("OPR")).split(" ")[0]).replace("/", "-")).replace("OPR", "Opera");
+        } else if (user.contains("chrome"))
+        {
+            browser=(userAgent.substring(userAgent.indexOf("Chrome")).split(" ")[0]).replace("/", "-");
+        } else if ((user.indexOf("mozilla/7.0") > -1) || (user.indexOf("netscape6") != -1)  || (user.indexOf("mozilla/4.7") != -1) || (user.indexOf("mozilla/4.78") != -1) || (user.indexOf("mozilla/4.08") != -1) || (user.indexOf("mozilla/3") != -1) )
+        {
+            //browser=(userAgent.substring(userAgent.indexOf("MSIE")).split(" ")[0]).replace("/", "-");
+            browser = "Netscape-?";
+
+        } else if (user.contains("firefox"))
+        {
+            browser=(userAgent.substring(userAgent.indexOf("Firefox")).split(" ")[0]).replace("/", "-");
+        } else if(user.contains("rv"))
+        {
+            browser="IE-" + user.substring(user.indexOf("rv") + 3, user.indexOf(")"));
+        } else
+        {
+            browser = "UnKnown, More-Info: "+userAgent;
+        }        
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
             out.println("<title>Servlet ExperimentServlet</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet ExperimentServlet at " + request.getContextPath() + "</h1>");
+            out.println("<h1 style='text-align: center'> Ma première servlet </h1>");
+            
+            out.println("<h2>Information concernant l'adresse IP et le navigateur utilisé par le client:</h2>");            
+            out.println("<p>Adresse IP du client: " + request.getRemoteAddr() +"</p>");
+            out.println("<p>Navigateur du client: " + browser +"</p>");
             out.println("</body>");
             out.println("</html>");            
         }
